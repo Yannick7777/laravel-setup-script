1274672a86a91607b1f475c858f100f90f66ccef
Yannick7777 <87943421+Yannick7777@users.noreply.github.com>
2025-01-29 16:49:01 +0100

    temporary fix to permission issues

--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 
 echo "Container build in progress.."
 composer_docker_hash=$(docker build -q .)
-docker run --rm -v ./laravel:/var/www/html -it $composer_docker_hash composer install
+docker run --user 1000:1000 --rm -v ./laravel:/var/www/html -it $composer_docker_hash composer install
 cp env_docker laravel/.env
 cp env_docker .env
 docker compose up -d
 sleep 5
-docker compose exec app chown www-data:www-data /var/www -R
 docker compose exec app php artisan key:generate
 docker compose exec app php artisan config:cache
 while true; do sleep 3s && docker compose exec app php artisan migrate:fresh && break; done
